8891e60174bc30189184772f848cdca4d780aa8f
francdomain <cfrancmail@gmail.com>
2025-09-23 03:13:08 +0100

    week2 task done.

--- frontend/src/components/RouteTracker.js
+++ frontend/src/components/RouteTracker.js
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useMonitoringWithRoutes } from '../hooks/useMonitoring';
 
 const RouteTracker = () => {
   // This component uses the route-aware monitoring hook
   useMonitoringWithRoutes();
-  
+
   return null; // This component doesn't render anything
 };
 
